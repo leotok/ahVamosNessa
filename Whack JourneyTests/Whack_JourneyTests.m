6de59523fc2a12b62415ad52d7aacfcfcafb9f93
leotok <leotok@hotmail.com>
2015-03-23 11:39:14 -0300

    Primeiro commit

---
+++ Whack JourneyTests/Whack_JourneyTests.m
@@ -0,0 +1,40 @@
+//
+//  Whack_JourneyTests.m
+//  Whack JourneyTests
+//
+//  Created by Jordana Mecler on 11/02/15.
+//  Copyright (c) 2015 Jordana Mecler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Whack_JourneyTests : XCTestCase
+
+@end
+
+@implementation Whack_JourneyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
