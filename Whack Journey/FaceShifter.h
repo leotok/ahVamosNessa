cc228a38667cbc10bccc2f67ca67b4d3b04e7439
leotok <leotok@hotmail.com>
2015-03-23 11:51:55 -0300

    4

---
+++ Whack Journey/FaceShifter.h
@@ -0,0 +1,17 @@
+//
+//  FaceShifter.h
+//  Whack Journey
+//
+//  Created by Leonardo Edelman Wajnsztok on 24/02/15.
+//  Copyright (c) 2015 Jordana Mecler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceShifter : UIImageView
+
+@property int faceShifterAcertou;
+@property int faceShifterCount;
+@property NSTimer *removeFaceShifterTimer;
+
+@end
---
+++ Whack Journey/FaceShifter.m
@@ -0,0 +1,13 @@
+//
+//  FaceShifter.m
+//  Whack Journey
+//
+//  Created by Leonardo Edelman Wajnsztok on 24/02/15.
+//  Copyright (c) 2015 Jordana Mecler. All rights reserved.
+//
+
+#import "FaceShifter.h"
+
+@implementation FaceShifter
+
+@end
